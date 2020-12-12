4b9f172e8801fe746ee036c6a76d050284529ddf
indranil2507 <iindranilsaha1@gmail.com>
2020-12-12 17:55:24 +0530

    event

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const tutorial = require('./tutorial');
-console.log(tutorial)
-console.log(tutorial(1,1));
+
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathobject());
