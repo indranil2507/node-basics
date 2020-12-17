37613f48c86b852593b55c3be6fa17ec44de91c6
indranil2507 <iindranilsaha1@gmail.com>
2020-12-18 02:14:10 +0530

    route parameters and query strings

--- gettin_started_expressjs/app.js
+++ gettin_started_expressjs/app.js
@@ -5,4 +5,15 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
+app.get('/example',(req,res)=>{
+    res.send('hitting the example route')
+})
+
+app.get('/example/:name/:age',(req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params.name + ":" + req.params.age);
+
+})
+
 app.listen(3000);
